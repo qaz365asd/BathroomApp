b5f22dcdd510d26fa89841270942dd2a27ef6f50
ckanelin <lincandy.yite@gmail.com>
2022-03-26 12:42:39 -0700

    prettifier

--- MainMap/AddButton.tsx
+++ MainMap/AddButton.tsx
@@ -1,42 +1,39 @@
-
-import * as React from 'react';
-import { View, Button, StyleSheet, TouchableOpacity, Text, StyleProp, TextStyle } from "react-native";
+import * as React from "react";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
 
 const styles = StyleSheet.create({
-    screenContainer: {
-        flex: 1,
-        justifyContent: "center",
-        padding: 16
-    },
-    appButtonContainer: {
-        elevation: 8,
-        backgroundColor: "#009688",
-        borderRadius: 10,
-        paddingVertical: 10,
-        paddingHorizontal: 12
-    },
-    appButtonText: {
-        fontSize: 18,
-        color: "#fff",
-        fontWeight: "bold",
-        alignSelf: "center",
-        textTransform: "uppercase"
-    }
+   screenContainer: {
+      flex: 1,
+      justifyContent: "center",
+      padding: 16,
+   },
+   appButtonContainer: {
+      elevation: 8,
+      backgroundColor: "#009688",
+      borderRadius: 10,
+      paddingVertical: 10,
+      paddingHorizontal: 12,
+   },
+   appButtonText: {
+      fontSize: 18,
+      color: "#fff",
+      fontWeight: "bold",
+      alignSelf: "center",
+      textTransform: "uppercase",
+   },
 });
 
 interface AddButtonProps {
-    onPress: () => void;
-    title: String;
+   onPress: () => void;
+   title: String;
 }
 
-const AppButton: React.FC<AddButtonProps> = ({ onPress, title})  => {
-    
-    return (
-        <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
-            <Text style={styles.appButtonText}>{title}</Text>
-        </TouchableOpacity>
-    );
-
+const AppButton: React.FC<AddButtonProps> = ({ onPress, title }) => {
+   return (
+      <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+         <Text style={styles.appButtonText}>{title}</Text>
+      </TouchableOpacity>
+   );
 };
 
 export default AppButton;
