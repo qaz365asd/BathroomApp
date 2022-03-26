4e5565b30de2a3d1ce89ad2d239f24e4e5e2544b
ckanelin <lincandy.yite@gmail.com>
2022-03-26 12:25:32 -0700

    Add marker onPress and add on press

--- App.tsx
+++ App.tsx
@@ -1,81 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Dimensions, Text} from 'react-native';
-import { useState } from 'react';
-import * as React from 'react';
-import MapView, {Marker} from 'react-native-maps';
-import AddButton from './MainMap/AddButton';
+import { StyleSheet, View, Dimensions, Alert } from "react-native";
+import { useState } from "react";
+import * as React from "react";
+import MapView, { Marker } from "react-native-maps";
+import AddButton from "./MainMap/AddButton";
 
-const defaultMarkers = [{latitude: 49.2827, longitude: -123.1207},{latitude: 49.4, longitude: -123.15}];
+const defaultMarkers = [
+   { latitude: 49.2827, longitude: -123.1207 },
+   { latitude: 49.4, longitude: -123.15 },
+];
 
 export default function App(this: any) {
    const [markers, setMarkers] = useState(defaultMarkers);
    const [isMain, setIsMain] = useState(true);
 
-  return (
-    <View  style={styles.container}>
-      {isMain ?
-        <View>
-          <MapView style={styles.map}
-          initialRegion={{
-            latitude: 49.2827,
-            longitude: -123.1207,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}>
-            {markers.map((marker, index) => (
-              <Marker
-                key={index}
-                coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
-              />))}
-          </MapView>
-          <View style={styles.buttonContainer}>
-            <AddButton onPress={()=>{setIsMain(!isMain)}} title={"Add"}/>
-          </View>
-        </View>  :
-        <View></View>
-      }
-      {/* { isMain 
-      ? 
+   if (isMain) {
+      return (
+         <View style={styles.container}>
+            <MapView
+               style={styles.map}
+               initialRegion={{
+                  latitude: 49.2827,
+                  longitude: -123.1207,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+               }}
+            >
+               {markers.map((marker, index) => (
+                  <Marker
+                     key={index}
+                     coordinate={{
+                        latitude: marker.latitude,
+                        longitude: marker.longitude,
+                     }}
+                     onPress={(e) => {
+                        // alert(marker);
+                     }}
+                  />
+               ))}
+            </MapView>
+            <View style={styles.buttonContainer}>
+               <AddButton
+                  onPress={() => {
+                     setIsMain(!isMain);
+                  }}
+                  title={"Add"}
+               />
+            </View>
+         </View>
+      );
+   }
+
+   return (
       <View style={styles.container}>
-        <MapView style={styles.map}
-        initialRegion={{
-          latitude: 49.2827,
-          longitude: -123.1207,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}>
-          {markers.map((marker, index) => (
-            <Marker
-              key={index}
-              coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
-            />))}
-        </MapView>
-        <View style={styles.buttonContainer}>
-          <AddButton onPress={()=>{setIsMain(!isMain)}} title={"Add"}/>
-        </View>
-      </View> 
-      :
-      null
-      } */}
-    </View>
-  );
+         <AddButton
+            onPress={() => {
+               setIsMain(!isMain);
+            }}
+            title={"Go Back"}
+         />
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 50
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  buttonContainer:{
-    position: 'absolute',
-    bottom: 20,
-    right: 20
-  }
+   container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingTop: 50,
+   },
+   map: {
+      width: Dimensions.get("window").width,
+      height: Dimensions.get("window").height,
+   },
+   buttonContainer: {
+      position: "absolute",
+      bottom: 20,
+      right: 20,
+   },
 });
