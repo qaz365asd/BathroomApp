89ee1e9898c2da1431f396e718216d805ad254f0
ckanelin <lincandy.yite@gmail.com>
2022-03-26 11:46:58 -0700

    add button

--- App.tsx
+++ App.tsx
@@ -1,19 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, Text} from 'react-native';
+import { useState } from 'react';
 import * as React from 'react';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
+import AddButton from './MainMap/AddButton';
 
-export default function App() {
+const defaultMarkers = [{latitude: 49.2827, longitude: -123.1207},{latitude: 49.4, longitude: -123.15}];
+
+export default function App(this: any) {
+   const [markers, setMarkers] = useState(defaultMarkers);
+   const [isMain, setIsMain] = useState(true);
+
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map}
-       initialRegion={{
-        latitude: 49.2827,
-        longitude: -123.1207,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}/>
-      <StatusBar style="auto" />
+    <View  style={styles.container}>
+      {isMain ?
+        <View>
+          <MapView style={styles.map}
+          initialRegion={{
+            latitude: 49.2827,
+            longitude: -123.1207,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+            {markers.map((marker, index) => (
+              <Marker
+                key={index}
+                coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+              />))}
+          </MapView>
+          <View style={styles.buttonContainer}>
+            <AddButton onPress={()=>{setIsMain(!isMain)}} title={"Add"}/>
+          </View>
+        </View>  :
+        <View></View>
+      }
+      {/* { isMain 
+      ? 
+      <View style={styles.container}>
+        <MapView style={styles.map}
+        initialRegion={{
+          latitude: 49.2827,
+          longitude: -123.1207,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+            />))}
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <AddButton onPress={()=>{setIsMain(!isMain)}} title={"Add"}/>
+        </View>
+      </View> 
+      :
+      null
+      } */}
     </View>
   );
 }
@@ -24,9 +67,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50
   },
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
+  buttonContainer:{
+    position: 'absolute',
+    bottom: 20,
+    right: 20
+  }
 });
